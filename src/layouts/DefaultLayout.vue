a7e9a9d3f9a8578aacf8f7c1f1af4443f33d24e8
Bagus Nur Andiansyah <bagusnur@protonmail.com>
2025-11-09 09:04:56 +0800

    feat: add Navbar component

--- src/layouts/DefaultLayout.vue
+++ src/layouts/DefaultLayout.vue
@@ -1,6 +1,19 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import Navbar, { type MenuItem } from '@/components/ui/Navbar.vue'
+import Button from '@/components/form/Button.vue'
+
+const navbarItems: MenuItem[] = [
+  { label: 'Templates', url: '/' },
+  { label: 'GitHub', url: 'https://github.com/bagusNA', externalUrl: true, icon: 'lucide--github' },
+]
+</script>
 
 <template>
-  <p>Default</p>
+  <Navbar :items="navbarItems">
+    <template #actions>
+      <Button>Get Started</Button>
+    </template>
+  </Navbar>
+
   <slot></slot>
 </template>
