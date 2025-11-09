444c79613f5c4925f723626e5bf3dde0548a5220
Bagus Nur Andiansyah <bagusnur@protonmail.com>
2025-11-09 09:52:44 +0800

    feat: add HeroSection component

---
+++ src/components/sections/HeroSection.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+export interface HeroSectionProps {
+  title: string
+  subtitle?: string | null
+  imageUrl?: string | null
+}
+
+defineProps<HeroSectionProps>()
+</script>
+
+<template>
+  <section
+    class="grid grid-cols-1 sm:grid-cols-2 items-center gap-y-12 gap-x-8 sm:flex-row"
+  >
+    <div>
+      <h1 class="text-3xl sm:text-4xl md:text-5xl font-bold mb-2">{{ title }}</h1>
+      <p v-if="subtitle"
+         class="mb-4"
+      >{{ subtitle }}</p>
+
+      <slot/>
+    </div>
+
+    <div>
+      <img
+        src="@/assets/images/placeholder.png"
+        alt="App Preview"
+        class="h-full w-auto aspect-auto object-cover"
+      >
+    </div>
+  </section>
+</template>
--- src/pages/Landing.vue
+++ src/pages/Landing.vue
@@ -1,13 +1,14 @@
-<script
-  setup
-  lang="ts"
->
+<script setup lang="ts">
+import HeroSection from '@/components/sections/HeroSection.vue'
+import Button from '@/components/form/Button.vue'
 </script>
 
 <template>
-  <p class="font-bold">landing</p>
-  <div>
-    <RouterLink to="/">Home</RouterLink>
-    <RouterLink to="/app">App</RouterLink>
-  </div>
+  <HeroSection
+    title="Your free and open-source resume builder"
+    subtitle="Quickly create your own resume and CV. Free, forever."
+    class="mt-12 sm:mt-18 md:mt-24 mb-12 max-w-5xl px-4 mx-auto"
+  >
+    <Button> Start building your resume </Button>
+  </HeroSection>
 </template>
