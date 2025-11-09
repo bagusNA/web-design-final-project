1db7cce2a8c34c66dbaf3343acea1cedf67837e6
Bagus Nur Andiansyah <bagusnur@protonmail.com>
2025-11-09 09:04:16 +0800

    feat: add Icon component

---
+++ src/components/ui/Icon.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+export interface IconProps {
+  name: string
+  // See: https://iconify.design/docs/usage/css/tailwind/tailwind4/#extra-class-name
+  color?: boolean
+}
+
+defineProps<IconProps>()
+</script>
+
+<template>
+  <i :class="[color ? 'iconify-color' : 'iconify', name]"></i>
+</template>
