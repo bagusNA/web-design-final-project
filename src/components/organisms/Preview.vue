340fb98ba74ddff32ff3f1f3c765fdd02da3e916
Bagus Nur Andiansyah <bagusnur@protonmail.com>
2025-12-07 20:05:29 +0800

    feat: preview control buttons

--- src/components/organisms/Preview.vue
+++ src/components/organisms/Preview.vue
@@ -4,19 +4,63 @@ import 'vue-zoomable/dist/style.css'
 
 import type { EditorData } from '@/components/organisms/EditorBar.vue'
 import SimpleTemplate from '@/components/organisms/templates/SimpleTemplate.vue'
+import { ref } from 'vue'
+import Button from 'primevue/button'
 
 export interface PreviewProps {
   editorData: EditorData
 }
 
 const props = defineProps<PreviewProps>()
+
+const defaultZoom = 0.8
+const zoomStep = 0.1
+const zoom = ref(defaultZoom)
+
+const defaultPan = { x: 0, y: 0 }
+const pan = ref(defaultPan)
+
+const zoomIn = () => {
+  zoom.value += zoomStep
+}
+const zoomOut = () => {
+  zoom.value -= zoomStep
+}
+const resetZoomPan = () => {
+  zoom.value = defaultZoom
+  pan.value = { ...defaultPan }
+}
 </script>
 
 <template>
-  <VueZoomable :enableControlButton="false" class="h-screen w-screen">
+  <VueZoomable
+    v-model:zoom="zoom"
+    v-model:pan="pan"
+    :enableControlButton="true"
+    :dblClickEnabled="false"
+    class="h-screen w-screen"
+  >
     <div class="A4-preview text-black bg-white">
       <SimpleTemplate :data="editorData" />
     </div>
+
+    <template #buttons>
+      <div class="absolute bottom-24 md:bottom-0 right-0 px-4 py-3 flex flex-col gap-2">
+        <!-- Reset zoom & pan -->
+        <Button
+          icon="iconify lucide--refresh-ccw"
+          severity="secondary"
+          class="shadow"
+          @click="resetZoomPan"
+        />
+
+        <!-- Zoom In -->
+        <Button icon="iconify lucide--plus" severity="secondary" class="shadow" @click="zoomIn" />
+
+        <!-- Zoom Out -->
+        <Button icon="iconify lucide--minus" severity="secondary" class="shadow" @click="zoomOut" />
+      </div>
+    </template>
   </VueZoomable>
 </template>
 
